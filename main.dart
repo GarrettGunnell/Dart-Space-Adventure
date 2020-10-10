159b70f48736e79f09850273110ce40bb939bb50
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-10-10 00:58:34 -0700

    Get random travel choice through stdin

--- main.dart
+++ main.dart
@@ -8,7 +8,15 @@ void main() {
     print("Nice to meet you, ${name}.");
     print("Let's go on an adventure.");
     print("Shall I randomly choose a planet for you? (Y / N)");
-    print("Name the planet you would like to visit.");
+    String randomChoice = stdin.readLineSync();
+    switch (randomChoice) {
+        case 'Y':
+            print("Traveling..");
+            break;
+        case 'N':
+            print("Name the planet you would like to visit");
+            break;
+    }
     print("Traveling to Earth");
     print("The shattered remains of this former planet can be found drifting through space, or in souvenir shops around the galaxy.");
 }
